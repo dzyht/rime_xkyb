a52994b82569a4219d7335bd51de3273cd1c2a5e
dzyht <iamdzyht@gmail.com>
2018-04-04 11:28:57 +0800

    创建

--- tools/1ybSortHeader.sh
+++ tools/1ybSortHeader.sh
@@ -1,8 +1,8 @@
-cp -rf ../rime/xkybcz.dict.yaml ../backdict/
-cp -rf ../rime/xkybdz.dict.yaml ../backdict/
-cp -rf ../rime/xkybyw.dict.yaml ../backdict/
-cp -rf ../rime/xkybjm.dict.yaml ../backdict/
-git add ../backdict/xkybcz.dict.yaml ../backdict/xkybdz.dict.yaml ../backdict/xkybyw.dict.yaml ../rime/xkybjm.dict.yaml
+cp -rf ../rime/xkybcz.dict.yaml ../BackDict/
+cp -rf ../rime/xkybdz.dict.yaml ../BackDict/
+cp -rf ../rime/xkybyw.dict.yaml ../BackDict/
+cp -rf ../rime/xkybjm.dict.yaml ../BackDict/
+git add ../BackDict/xkybcz.dict.yaml ../BackDict/xkybdz.dict.yaml ../BackDict/xkybyw.dict.yaml ../rime/xkybjm.dict.yaml
 git commit -m "更新：备份旧词库"
 echo 开始处理排序
 sort -t $'\t' -k 2 词组.txt > xkybcz.dict.yaml
@@ -14,10 +14,10 @@ cp -rf xkybdz.dict.yaml ./单字.txt
 cp -rf xkybyw.dict.yaml ./英文.txt
 cp -rf xkybjm.dict.yaml ./简码.txt
 echo 更为当前文件
-sed -i 's/\b并不比\b\tbbb/---\nname: xkybcz\nversion: "V1"\nsort: by_xkyb\n...\n&/' xkybcz.dict.yaml
-sed -i 's/\b不\b\tb/---\nname: xkybdz\nversion: "V1"\nsort: by_xkyb\n...\n&/' xkybdz.dict.yaml
-sed -i 's/\bbiangbiang面\b\tbbmmu/---\nname: xkybyw\nversion: "V1"\nsort: by_xkyb\n...\n&/' xkybyw.dict.yaml
-sed -i 's/\不能\b\tba/---\nname: xkybjm\nversion: "V1"\nsort: by_xkyb\n...\n&/' xkybjm.dict.yaml
+sed -i 's/\b爸爸\b\tbaba/---\nname: xkybcz\nversion: "Q1"\nsort: by_weight\n...\n&/' xkybcz.dict.yaml
+sed -i 's/\b那\b\ta/---\nname: xkybdz\nversion: "Q1"\nsort: by_weight\n...\n&/' xkybdz.dict.yaml
+sed -i 's/\bbiangbiang面\b\tbbmf/---\nname: xkybyw\nversion: "Q1"\nsort: by_weight\n...\n&/' xkybyw.dict.yaml
+sed -i 's/\b不错\b\tbc/---\nname: xkybjm\nversion: "Q1"\nsort: by_weight\n...\n&/' xkybjm.dict.yaml
 echo 执行排序添加头部信息完成
 cp -rf xkybcz.dict.yaml ../rime/
 cp -rf xkybdz.dict.yaml ../rime/
