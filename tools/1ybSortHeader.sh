816290e9f9b477b14a68fb6a2199ba1d23a007ff
dzyht <iamdzyht@gmail.com>
2018-04-30 11:29:40 +0800

    0430

--- tools/1ybSortHeader.sh
+++ tools/1ybSortHeader.sh
@@ -1,8 +1,8 @@
-cp -rf ../rime/xkybcz.dict.yaml ../BackDict/
-cp -rf ../rime/xkybdz.dict.yaml ../BackDict/
-cp -rf ../rime/xkybyw.dict.yaml ../BackDict/
-cp -rf ../rime/xkybjm.dict.yaml ../BackDict/
-git add ../BackDict/xkybcz.dict.yaml ../BackDict/xkybdz.dict.yaml ../BackDict/xkybyw.dict.yaml ../rime/xkybjm.dict.yaml
+cp -rf ../xkybcz.dict.yaml ../BackDict/
+cp -rf ../xkybdz.dict.yaml ../BackDict/
+cp -rf ../xkybyw.dict.yaml ../BackDict/
+cp -rf ../xkybjm.dict.yaml ../BackDict/
+git add ../BackDict/xkybcz.dict.yaml ../BackDict/xkybdz.dict.yaml ../BackDict/xkybyw.dict.yaml ../BackDict/xkybjm.dict.yaml
 git commit -m "更新：备份旧词库"
 echo 开始处理排序
 sort -t $'\t' -k 2 词组.txt > xkybcz.dict.yaml
@@ -19,8 +19,8 @@ sed -i 's/\b那\b\ta/---\nname: xkybdz\nversion: "Q1"\nsort: by_weight\n...\n&/'
 sed -i 's/\bAA制\b\taaz/---\nname: xkybyw\nversion: "Q1"\nsort: by_weight\n...\n&/' xkybyw.dict.yaml
 sed -i 's/\b不错\b\tbc/---\nname: xkybjm\nversion: "Q1"\nsort: by_weight\n...\n&/' xkybjm.dict.yaml
 echo 执行排序添加头部信息完成
-cp -rf xkybcz.dict.yaml ../rime/
-cp -rf xkybdz.dict.yaml ../rime/
-cp -rf xkybyw.dict.yaml ../rime/
-cp -rf xkybjm.dict.yaml ../rime/
+cp -rf xkybcz.dict.yaml ../
+cp -rf xkybdz.dict.yaml ../
+cp -rf xkybyw.dict.yaml ../
+cp -rf xkybjm.dict.yaml ../
 echo 已放置到主词库中
